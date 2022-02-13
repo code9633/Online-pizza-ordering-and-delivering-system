4bf449730d34e019624677ddb3b7a1891305070a
code9633 <shashikaudara5566@gmail.com>
2022-02-13 22:59:41 +0530

    First commit of the customer section

---
+++ front-section/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+
+<head>
+    <meta charset="UTF-8">
+
+    <!-- for mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="format-detection" content="telephone=no" />
+
+    <!-- Favicon Icon -->
+    <link rel="shortcut icon" href="../images/favicon.ico" type="image/vnd.microsoft.icon" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-ico" />
+    <title>Pizza Restaurant</title>
+
+    <link rel='stylesheet' href='rs-slider/css/settings.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/animsition.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/font-awesome.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/simple-line-icons.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/pe-icon-7-stroke.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/flaticon.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/owl.carousel.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/jquery.easy-pie-chart.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/owl.transitions.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/style.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/media.css' type='text/css' media='all' />
+    <link rel='stylesheet' href='css/custom_script.css' type='text/css' media='all' />
+
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Merriweather%3A%2C400%7CPatua+One%3A400&amp;ver=1.0.0' type='text/css' media='all' />
+    <link href="http://fonts.googleapis.com/css?family=Patua+One:400" rel="stylesheet" property="stylesheet" type="text/css" media="all">
+
+
+</head>
+
+<body class="home page-template page-template-template-home page-template-template-home-php page page-id-7">
+    <div class="animsition global-wrapper">
+        <div id="header" class="header-wrapper">
+            <div class="logo">
+                <a href="index.php"><img class="logoImage" src="images/logo.png"></a>
+                <div class="clear"></div>
+            </div>
+            <div class="menu-wrapper">
+                <div class="main-menu">
+                    <div class="menu-main-nav-menu-container">
+                        <ul id="menu-main-nav-menu" class="sf-menu">
+                            <li class="menu-item menu-item-home current-menu-item current_page_item"><a href="index.php">Home</a></li>
+                            <li class="menu-item"><a href="menu.php">Menu</a></li>
+                            <li class="menu-item"><a href="pizzas.php">Pizzas</a></li>
+                            <li class="menu-item"><a href="about.php">About us</a></li>
+                            <li class="menu-item"><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="menu-icons-inside">
+                    <div class="menu-icon menu-icon-mobile"><span class="menu-icon-create"></span></div>
+                </div>
+            </div>
+            <div class="clear"></div>
+            <div class="footer">
+                <div class="footer-socials">
+                    <ul class="socials-sh">
+                        <li>
+                            <a class="fa sh-socials-url fa-twitter" href="#" title="Twitter" target="_blank"></a>
+                        </li>
+                        <li>
+                            <a class="fa sh-socials-url fa-facebook" href="#" title="Facebook" target="_blank"></a>
+                        </li>
+                        <li>
+                            <a class="fa sh-socials-url fa-linkedin" href="#" title="LinkedIn" target="_blank"></a>
+                        </li>
+                        <li>
+                            <a class="fa sh-socials-url fa-google-plus" href="#" title="Google Plus" target="_blank"></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-content">
+                    @ 2022 Pizza Restuarant </div>
+            </div>
+        </div>
+
+        <div class="mobile-menu-wrapper">
+            <div class="menu-main-nav-menu-container">
+                <ul id="menu-main-nav-menu-1" class="mobile-menu">
+                    <li class="menu-item menu-item-home current-menu-items current_page_item"><a href="index.php">Home</a></li>
+                    <li class="menu-item"><a href="menu.php">Menu</a></li>
+                    <li class="menu-item"><a href="pizzas.php">Pizzas</a></li>
+                    <li class="menu-item"><a href="about.php">About us</a></li>
+                    <li class="menu-item"><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
--- index.php
+++ index.php
@@ -1,162 +1,52 @@
-<?php
- include "config.php";
+<?php include('front-section/header.php');?>
 
- session_start();
+
+    <div class="container-wrapper">
+        <div id="rev_slider_1_1_wrapper" class="rev_slider_wrapper fullscreen-container" data-source="gallery" style="background-color:transparent;padding:0px;">
+            <!-- START REVOLUTION SLIDER 5.3.1.5 fullscreen mode -->
+            <div id="rev_slider_1_1" class="rev_slider fullscreenbanner" style="display:none;" data-version="5.3.1.5">
+                <ul>
+                    <!-- SLIDE  -->
+                    <li data-index="rs-1" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="upload/index_image.jpg" data-rotate="0" data-saveperformance="off" data-title="Mexican" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <img src="upload/index_image.jpg" alt="" title="slide4" width="2560" height="1440" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Back.easeOut" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+                    </li>
+                    <!-- SLIDE  -->
+                    <li data-index="rs-2" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="upload/IndexImage02.jpg" data-rotate="0" data-saveperformance="off" data-title="Margherita" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <img src="upload/IndexImage02.jpg" alt="" title="bg-pizza" width="1316" height="822" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Linear.easeNone" data-scalestart="120" data-scaleend="100" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+                    </li>
+                    <!-- SLIDE  -->
+                    <li data-index="rs-3" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="300" data-thumb="upload/IndexImage04.jpg" data-rotate="0" data-saveperformance="off" data-title="Classic" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <img src="upload/IndexImage04.jpg" alt="" title="slide5" width="1800" height="1013" data-bgposition="center center" data-kenburns="on" data-duration="10000" data-ease="Back.easeOut" data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0" data-offsetstart="0 0" data-offsetend="0 0" class="rev-slidebg" data-no-retina>
+                        <!-- LAYERS -->
+                    </li>
+                </ul>
+                <div style="" class="tp-static-layers">
+
+                    <!-- LAYER NR. 1 -->
+                    <div class="tp-caption   tp-resizeme tp-static-layer" id="slider-1-layer-5" data-x="['center','center','center','center']" data-hoffset="['150','142','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-150','-148','-60','-40']" data-fontsize="['92','92','92','50']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on" data-startslide="0" data-endslide="2" data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"y:[-100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"nothing"}]' data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]" style="z-index: 5; white-space: nowrap; font-size: 92px; line-height: 92px; font-weight: 400; color: rgba(255, 255, 255, 1.00);font-family:Patua One;text-shadow:0px 2px 2px rgba(0, 0, 0, 0.6);">We have the Best </div>
+
+                    <!-- LAYER NR. 2 -->
+                    <div class="tp-caption   tp-resizeme tp-static-layer" id="slider-1-layer-6" data-x="['center','center','center','center']" data-hoffset="['0','110','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-16','-16','74','70']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on" data-startslide="0" data-endslide="2" data-frames='[{"delay":1500,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power4.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]' data-textAlign="['inherit','inherit','inherit','inherit']" data-paddingtop="[5,5,5,5]" data-paddingright="[40,40,40,40]" data-paddingbottom="[5,5,5,5]" data-paddingleft="[40,40,40,40]" style="z-index: 6; white-space: nowrap; font-size: 140px; line-height: 140px; font-weight: 400; color: rgba(250, 185, 64, 1.00);font-family:Patua One;background-color:rgba(36, 36, 36, 1.00);border-radius:10px 10px 10px 10px;">Pizza </div>
+                </div>
+
+                <div class="tp-bannertimer tp-bottom" style="visibility: hidden !important;"></div>
+            </div>
+
+        </div>
+        <!-- END REVOLUTION SLIDER -->
+        <div class="clear"></div>
+    </div>
+    <!-- end container-wrapper -->
+</div>
+
+<?php 
+
+include('front-section/script.php');
+include('front-section/footer.php');
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzaa - Home Page</title>
-    <link rel="stylesheet" href="css/index.css">
-</head>
-<body style="background-image: url(images/bg2.png);">
-    <!---------------------------- Menu section start----------------------------->
-    <div class="navigation-container">
-        <div class="navigation-content">
-            <div class="logo">
-                <img src="images/logo.png" alt="logo image">
-            </div>
-            <div class= "menu">
-                <ul>
-                    <li><a href="#">MENU</a></li>
-                    <li><a href="#">PROMOS</a></li>
-                    <li><a href="#">ONLINE ORDER</a></li>
-                </ul>            
-            </div>
-            <div class="user">
-                <?php 
-                    if (isset($_SESSION['username']) and !empty($_SESSION['username'])){
-                ?>
-                <a class="login-register-btn" href="#">Log Out</a>
-                 
-                <?php } else{?>  
-                <a class="login-register-btn" href="login.php">Sign In / Register</a>
-                <?php }?>       
-                
-            </div>
-
-            <div class="user-display-name">
-            <?php 
-                    if (isset($_SESSION['username']) and !empty($_SESSION['username'])){
-                ?>
-                <p class="user-name">Hi,<?php echo $_SESSION['username']; ?></p>
-                 
-                <?php } else{?>  
-                    <p>&nbsp;</p>
-                <?php }?> 
-
-            </div>
-
-            <div class="cart">
-                <img height="30px" src="images/cart.png">
-                <a href="#">Cart</a>
-            </div>
-            
-        </div>
-    </div>
-    <!------------------------ Menu section end -------------------------------------------->
-
-    <!------------------- Main content section start---------------------------------------- -->
-    <div class="content-container">
-        <div class="whats-new">
-            <img src="images/WHATS NEW.png">
-        </div>
-        <div class="content">
-            <div class="first-row">
-                <div class="new" style="background-image: url(images/new.png);">
-                    <p>GRILLED CHICKEN</p>
-                    <P>SUMMER PIZZA</P>
-                    <button>HOT & SPICY</button>
-                </div>
-                <div class="find">
-                    <div class="search" style="background-image: url(images/find.png);">
-                        <p>FIND LOCATION</p>
-                        <div class="text">
-                            <input type="text" placeholder="Search Something Here">
-                            <button>Search</button>
-                        </div>
-                    </div>
-                    <div class="order" style="background-image: url(images/order.png);">
-                        <a href="#"><font stytle = "color: yellow">ORDER </font>ONLINE</a>
-                    </div>
-                </div>
-            </div>
-            <div class="second-row">
-                <div class="food-one" style="background-image: url(images/food-1.png);">
-                    <div class="name">
-                        <p>VEGGIE</p>
-                        <P>SPECIAL</P>
-                    </div>
-                    <div class="price">
-                        <button>$9<sup>99</sup></button>
-                    </div>
-                </div>
-                <div class="food-two" style="background-image: url(images/food-2.png);">
-                    <div class="name">
-                        <p>CHICKEN WRAP</p>
-                        <P>SPECIAL</P>
-                    </div>
-                    <div class="price">
-                        <button>$9<sup>00</sup></button>
-                    </div>
-                </div>
-                <div class="food-three" style="background-image: url(images/food-3.png);">
-                    <div class="name">
-                        <p>PRAWN</p>
-                        <P>SUMMER SPECIAL</P>
-                    </div>
-                    <div class="price">
-                        <button>$5<sup>00</sup></button>
-                    </div>
-                </div>
-            </div>
-            <div class="third-row">
-                <div class="flavor" style="background-image: url(images/flavor.png);">
-                    <p>FLAVOR MENU</p>
-                    <p>VEGETABLE HAND TOSED</p>
-                </div>
-                <div class="breakfast" style="background-image: url(images/breakfast.png);">
-                    <p>BREAKFAST MENU</p>
-                    <P>PARANTHA ROLL</P>
-                </div>
-            </div>    
-        </div>
-    </div>
-    <!-------------------------- Main section end ---------------------------------- -->
-
-    <!-------------------------- Footer section start-------------------------- -->
-    <div class="footer">
-        <div class="navigation-content">
-            <div class="logo"> 
-                <img src="images/logo.png">
-            </div>
-            <div class="menu footer-menu">
-                <ul>
-                    <li><a href="#">ABOUT US</a></li>
-                    <li><a href="#">CONTACT US</a></li>
-                    <li><a href="#">TEAM</a></li>
-                    <li><a href="#">SUPPORT</a></li>
-                </ul>
-            </div>
-            <div class="social">
-                <ul>
-                    <li><a href="#"><img src="images/facebook.png" alt=""></a></li>
-                    <li><a href="#"><img src="images/twitter.png" alt=""></a></li>
-                    <li><a href="#"><img src="images/youtube.png" alt=""></a></li>
-                </ul>                 
-            </div>
-        </div>
-          
-        <div class="copyright">
-            <p>Copyright 2021 Things.Going To Internet</p>
-        </div>
-    </div>  
-    <!-- ------------------------- Footer section end ------------------------------------ -->
-</body>
-</html>
